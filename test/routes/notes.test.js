c10805f9946a97538124d450423632ebccb294a6
Ryan Mehta <ryan.mehta@gmail.com>
2019-03-18 14:59:26 -0700

    notes get routes

--- test/routes/notes.test.js
+++ test/routes/notes.test.js
@@ -1,23 +1,9 @@
-require('dotenv').config();
+const { getNote } = require('../dataHelpers');
 
-const connect = require('../../lib/utils/connect');
-const mongoose = require('mongoose');
 const app = require('../../lib/app');
 const request = require('supertest');
 
 describe('notes routes', () => {
-  beforeAll(() => {
-    return connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
   it('creates a new note when posted to', () => {
     return request(app)
       .post('/notes')
@@ -28,6 +14,31 @@ describe('notes routes', () => {
           _id: expect.any(String),
           title: 'My Note',
           body: 'Notes are hard to write',
+          __v: 0
+        });
+      });
+  });
+
+  it('get a list of notes', () => {
+    return request(app)
+      .get('/notes')
+      .then(res => {
+        expect(res.ok).toBeTruthy();
+        expect(res.body).toHaveLength(5);
+      });
+  });
+
+  it('get a note by id', async() => {
+    const { _id } = await getNote();
+
+    return request(app)
+      .get(`/notes/${_id}`)
+      .then(res => {
+        expect(res.ok).toBeTruthy();
+        expect(res.body).toEqual({
+          _id,
+          title: 'My Note0',
+          body: 'My Note 0',
           __v: 0
         });
       });
