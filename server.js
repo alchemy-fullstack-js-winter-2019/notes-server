20293d67525eaa45d77964643535724c840d06df
Ryan Mehta <ryan.mehta@gmail.com>
2019-03-29 09:53:07 -0700

    bump

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = require('./lib/app');
 const PORT = process.env.PORT || 7891;
 
 app.listen(PORT, () => {
-  console.log('Server started');
+  console.log(`Server started on ${PORT}`);
 });
